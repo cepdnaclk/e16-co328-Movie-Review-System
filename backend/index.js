5c6af853da63be47d1753020e18a5f938a93b21f
DamsyDeSilva <damsydesilva@gmail.com>
2021-10-27 04:08:33 +0530

    Mongodb atlas connection added

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import dotenv from 'dotenv';
 import cors from 'cors';
+import mongoose from "mongoose";
 
 import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
+const CONNECTION_URL = process.env.MONGO_ATLAS_URI
 
 app.use(express.json());
 app.use(cors());
@@ -16,6 +18,12 @@ app.use("/users", userRoutes);
 app.get("/", (req, res) => res.send("Welcome to the Movie-Review API!"));
 app.all("*", (req, res) => res.send("Route doesn't exist."));
 
-app.listen(PORT, () =>
-    console.log(`Server running on PORT : http://localhost:${PORT}`)
-);
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        app.listen(PORT, () =>
+            console.log(`Server running on PORT : ${PORT}`))
+    })
+    .catch((error) => console.log(`${error} did not connect`));
+
+export {app};
