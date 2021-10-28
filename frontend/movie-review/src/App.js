f84a9817cf0a28b1d51b1a7da4db61ed4889b9a5
ChaminieDesilva <e16070@eng.pdn.ac.lk>
2021-10-28 17:22:05 +0530

    initial steps and header

---
+++ frontend/movie-review/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SearchAppBar from './components/header';
+
+function App() {
+  return (
+    <div >
+      <SearchAppBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/movie-review/src/components/header.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import { fade,makeStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Button, InputBase, Typography } from '@material-ui/core/';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyle = makeStyles((theme) => ({
+
+    header: {
+        flexGrow: 1,
+    },
+    bar: {
+        background: '#051726'
+    },
+    title: {
+        flexGrow: 1,
+        display: 'none',
+        fontSize: 25,
+        fontWeight: "bolder",
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+    login: {
+        margin: 20,
+        fontWeight: "bolder",
+        [theme.breakpoints.down('sm')]: {
+            padding: 5,
+        },
+    },
+    search: {
+        position: 'relative',
+        borderRadius: 20,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(1),
+            width: 'auto',
+        },
+    },
+    searchicon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    avatar: {
+        marginRight: 20,
+        border: "2px solid white"
+    },
+    user: {
+        margin: 25
+    },
+    gh: {
+        color: "white",
+        margin: 15,
+        [theme.breakpoints.down('sm')]: {
+            display: "none"
+        }
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '20ch',
+            '&:focus': {
+                width: '30ch',
+            },
+        },
+    },
+}));
+
+export default function SearchAppBar() {
+    const classes = useStyle();
+
+    return (
+        <div className={classes.header}>
+            <AppBar position='static' className={classes.bar}>
+                <Toolbar>
+                    <Typography className={classes.title} variant="h6" noWrap>
+                        MOVIE REVIEW 
+                    </Typography>
+                    <div className={classes.search}>
+                        <div className={classes.searchicon}>
+                            <SearchIcon />
+                        </div>
+                        <InputBase
+                            placeholder="Search Movie"
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput,
+                            }}
+                        />
+                    </div>
+                    <Button variant="contained" className={classes.login}>Log In</Button>
+                </Toolbar>
+            </AppBar>
+
+        </div>
+    );
+
+}
