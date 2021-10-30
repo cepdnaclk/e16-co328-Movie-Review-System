b9f07401f7ee382b777d61ce27c1cd8bd1c5638f
ChaminieDesilva <e16070@eng.pdn.ac.lk>
2021-10-30 20:33:19 +0530

    cast review list of user

--- frontend/movie-review/src/components/userReviews.js
+++ frontend/movie-review/src/components/userReviews.js
@@ -1,7 +1,7 @@
 import { React, useEffect, useState } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import { baseUrl } from "../shared/baseUrl";
-import { Typography, Link, List, ListItem, Button} from '@material-ui/core';
+import { Typography, Link, List, ListItem, Button } from '@material-ui/core';
 import Rating from '@material-ui/lab/Rating';
 
 const useStyles = makeStyles((theme) => ({
@@ -49,7 +49,8 @@ const useStyles = makeStyles((theme) => ({
 
 export default function UserReviewList(props) {
     const [movieReviews, setMovieRevies] = useState(0);
-    
+    const [castReviews, setCastRevies] = useState(0);
+
     const classes = useStyles();
 
     useEffect(() => {
@@ -61,24 +62,45 @@ export default function UserReviewList(props) {
             .then(response => setMovieRevies(response))
             .catch(() => window.location.href = "/");
 
-        
+        fetch(baseUrl + "cast-review/" + JSON.parse(window.localStorage.getItem("user"))._id + "/list",
+            {
+                headers: { 'Authorization': 'Bearer ' + window.localStorage.getItem("token") }
+            })
+            .then(response => response.json())
+            .then(response => setCastRevies(response))
+            .catch(() => window.location.href = "/");
+
+
 
     }, [])
 
     return (
-        
+
         <div>
             <Typography className={classes.heading}>Movie Reviews:</Typography>
             <List component="nav" className={classes.list}>
-                {movieReviews ? movieReviews.map(review => ( 
+                {movieReviews ? movieReviews.map(review => (
                     <ListItem button>
-                         <Typography className={classes.text}>{review.content}</Typography>
-                         <Typography className={classes.box}><Rating name= "rating" readOnly value={parseInt(review.rating)} /></Typography>
-                          <Link href= {"/movie/" + review.movieId} className={classes.link} >
-                          <Button className={classes.btn}>Movie Details</Button>
-                          </Link> 
+                        <Typography className={classes.text}>{review.content}</Typography>
+                        <Typography className={classes.box}><Rating name="rating" readOnly value={parseInt(review.rating)} /></Typography>
+                        <Link href={"/movie/" + review.movieId} className={classes.link} >
+                            <Button className={classes.btn}>Movie Details</Button>
+                        </Link>
                     </ListItem>
-                ) ) : "" }
+                )) : ""}
+            </List>
+
+            <Typography className={classes.heading}>Cast Reviews:</Typography>
+            <List component="nav" className={classes.list}>
+                {castReviews ? castReviews.map(review => (
+                    <ListItem button>
+                        <Typography className={classes.text}>{review.content}</Typography>
+                        <Typography className={classes.box}><Rating name="rating" readOnly value={parseInt(review.rating)} /></Typography>
+                        <Link href={"/people/" + review.personId} className={classes.link} >
+                            <Button className={classes.btn}>Cast Details</Button>
+                        </Link>
+                    </ListItem>
+                )) : ""}
             </List>
         </div>
     );
